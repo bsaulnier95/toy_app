69eb7c18cfac8ce5e2527065b02a22159cb6cdeb
Ben Saulnier <bsaulnier95@gmail.com>
2021-12-09 00:13:42 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.hosts << "hostname"
+    # Whitelist a test domain. Rails adds \A and \z around
+    # your regular expressions.
+    config.hosts << /application\.local/
+    config.cache_classes = false
+  
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "8244de2d98674d3a920b3a307e5589b3.vfs.cloud9.us-east-2.amazonaws.com"
 end
